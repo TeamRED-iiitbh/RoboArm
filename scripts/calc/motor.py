c57f145593958444c369dc64bf36316963ddd090
shrijal2410 <shrijal2410@gmail.com>
2023-12-20 19:17:59 +0530

    update: readme.md

--- scripts/calc/motor.py
+++ scripts/calc/motor.py
@@ -1,39 +1,30 @@
-"""
---------------------------------------------------------------------------------
-Stepper Motor Calculator
---------------------------------------------------------------------------------
-
-# Author: Shrijal
-"""
-
 import math
 
 
 class StepperMotor:
     """
-    # Attributes:
+    Stepper Motor Calculator
+
+    Author: Shrijal
+
+    Attributes:
+    - name: Unique identifier for the motor object.
     - model: The model number or part name.
-    - size: frame size (e.g., NEMA23, NEMA17).
-    - stepAngle: step angle  in degrees.
-    - ratedVoltage: tested voltage from T-Curve in V.
-    - ratedCurrent: phase current in A.
-    - resistance: phase resistance in Ω with ±10%.
-    - inductance: in (mH) at 1 kHz with ±20%.
+    - stepAngle: Step angle in degrees.
+    - ratedVoltage: Tested voltage from T-Curve in V.
+    - ratedCurrent: Phase current in A.
+    - resistance: Phase resistance in Ω with ±10%.
+    - inductance: Inductance in (mH) at 1 kHz with ±20%.
     - holdingTorque: The holding torque of the motor in Nm.
 
-    # Methods:
-    - power(): at rated voltage and current.
-    - max_power(): that motor can handle at +20% safety margin.
-    - max_speed(): max speed at tested voltage.
-    - min_delay(): min delay in (ms) to reach max speed.
-
-    # References:
-    - https://www.geckodrive.com/support/power-supply-basics/
-    - https://www.allaboutcircuits.com/tools/stepper-motor-calculator/
-
+    Methods:
+    - power(): Calculates average and maximum power at rated voltage and current.
+    - speed(): Calculates minimum and maximum speed at tested voltage for specified steps/revolution.
+    - min_delay(): Calculates minimum delay in (s) and (ms) to reach maximum speed.
     """
 
-    def create(self, model, deg, V, A, ohm, mH, Nm):
+    def __init__(self, name, model, deg, V, A, ohm, mH, Nm):
+        self.name = name
         self.model = model
         self.stepAngle = deg
         self.ratedVoltage = V
@@ -43,58 +34,41 @@ class StepperMotor:
         self.holdingTorque = Nm
 
     def power(self):
-        effective_current = 2 / 3 * self.ratedCurrent
-        return effective_current * self.ratedVoltage
+        avg_power = (2 / 3) * self.ratedCurrent * self.ratedVoltage
+        max_voltage = min(math.sqrt(self.inductance * 1000) * 32, 50)
+        max_power = 1.2 * self.ratedCurrent * max_voltage
+        return avg_power, max_power, max_voltage
 
-    def max_power(self):
-        max_current = self.ratedCurrent
-        max_voltage = math.sqrt(self.inductance * 1000) * 32
-        return max_current * max_voltage * 1.2
-
-    def max_speed(self):
-        spr = 25000  # steps per revolution
-        speed = 60 / (2 * self.inductance * self.ratedCurrent * 0.001 * spr)
-        return self.ratedVoltage * speed
+    def speed(self):
+        spr_values = [400, 25000]
+        factor = 60 / (2 * self.inductance * self.ratedCurrent)
+        min_speed, max_speed = [self.ratedVoltage * factor / spr for spr in spr_values]
+        return min_speed, max_speed
 
     def min_delay(self):
-        max_voltage = math.sqrt(self.inductance) * 32
-        delay = 2 * self.inductance * self.ratedCurrent * 0.001 / max_voltage
-        return delay
+        delay_s = (2 * self.inductance * self.ratedCurrent) / self.ratedVoltage
+        delay_ms = delay_s * 1000
+        return delay_s, delay_ms
+
+    def __str__(self):
+        avg_power, max_power, max_voltage = self.power()
+        min_speed, max_speed = self.speed()
+        delay_s, delay_ms = self.min_delay()
+        result = (
+            f"{self.name} Stepper Motor\n"
+            "--------------------------------------------------------------------------------\n"
+            f"Rated Voltage: {self.ratedVoltage} V, Rated Current: {self.ratedCurrent} A\n"
+            f"Power: {avg_power:.2f} - {max_power:.2f} W @ {max_voltage:.2f} V\n"
+            f"Speed: {max_speed:.2f} - {min_speed:.2f} RPM\n"
+            f"Min Delay: {delay_s:.6f} s, {delay_ms:.2f} ms\n"
+            "--------------------------------------------------------------------------------\n"
+        )
+        return result
 
 
-"""
---------------------------------------------------------------------------------
-Motor Specifications:
---------------------------------------------------------------------------------
-    1. [1] [1151779] 57HS112-4204 NEMA23 31 kg-cm Stepper Motor - D-Type Shaft
-        Buy: https://robu.in/product/neema-23-jk57hs112-4204-3-1n-m-d-type/
-        Datasheet: https://www.steppermotorcanada.ca/57hs112-4204-03.pdf
+NEMA23_30 = StepperMotor("NEMA23_30", "JK57HS112-4204", 1.8, 36, 4.2, 0.9, 3.8, 3.1)
+NEMA23_18 = StepperMotor("NEMA23_18", "57HS76-2804-05", 1.8, 30, 2.8, 1.13, 3.6, 1.89)
+NEMA17_07 = StepperMotor("NEMA17_07", "42HS60-1684", 1.8, 24, 1.68, 1.7, 2.8, 0.72)
+NEMA17_05 = StepperMotor("NEMA17_05", "KS42BYGHW811", 1.8, 24, 2.5, 1.25, 1.8, 0.48)
 
-    2. [2] [6727] 57HS76-2804-05 NEMA23 18.9 kg-cm Hybrid Stepper Motor - D-Type Shaft
-        Buy: https://www.robu.in/product/57hs76-2804-05-nema23-18-9-kg-cm-hybrid-stepper-motor-d-type-shaft/
-        Datasheet: https://ecksteinimg.de/Datasheet/Schrittmotor/JK57HS76-2804/JK57HS76-2804.pdf
-        Dimensions: https://robu.in/wp-content/uploads/2015/12/NEMA-23-18.9-kg-cm-Hybrid-Stepper-Motor-ROBU.IN_.gif
-
-    3. [1] [1151778] 42HS60-1684 NEMA17 7.2 kg-cm Stepper Motor - Round Type Shaft
-        Buy: https://robu.in/product/neema-17-jk42hs60-1704-0-72n-m-round-type/
-        Datasheet: https://robu.in/wp-content/uploads/2023/07/1551713.pdf
-
-    4. [1] [1551713] 42HS60-1684 NEMA17 7.2Kg-cm Stepper Motor Round-Type Shaft
-        Buy: https://robu.in/product/42hs60-1684-nema17-7-2kg-cm-stepper-motor-round-type/
-        Datasheet: https://robu.in/wp-content/uploads/2023/07/1551713.pdf
-
-    5. [2] [1151777] JK42HS60-1206F NEMA17 6.5 kg-cm Stepper Motor - D-Type Shaft
-        Buy: https://robu.in/product/neema-17-jk42hs60-1206-0-65n-m-d-type/
-        Datasheet: https://robu.in/wp-content/uploads/2022/03/datasheet.pdf
-
-    6. [2] [106140] 42HS48-2504AF-01 NEMA17 4.8 kg-cm Stepper Motor With Detachable Cable - D-Type Shaft
-        Buy: https://robu.in/product/nema17-4-8-kg-cm-stepper-motor-with-detachable-72-cm-cable/
-        Datasheet: https://robu.in/wp-content/uploads/2023/04/JK42HS48-2504AF-01.pdf
-        Dimensions: https://robu.in/wp-content/uploads/2018/08/NEMA-17-Stepper-Motor-4.8-kg-cm-Dimensional-Drawing-ROBU.IN_.jpg
-            
-"""
-
-NEMA23_30 = StepperMotor.create("JK57HS112-4204", 1.8, 36, 4.2, 0.9, 3.8, 3.1)
-NEMA23_18 = StepperMotor.create("57HS76-2804-05", 1.8, 30, 2.8, 1.13, 3.6, 1.89)
-NEMA17_07 = StepperMotor.create("42HS60-1684", 1.8, 24, 1.68, 1.7, 2.8, 0.72)
-NEMA17_05 = StepperMotor.create("KS42BYGHW811", 1.8, 24, 2.5, 1.25, 1.8, 0.48)
+print(NEMA23_30, NEMA23_18, NEMA17_07, NEMA17_05)
